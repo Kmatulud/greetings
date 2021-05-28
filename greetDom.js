58f9fedfa807e17d956651c6c8f2b3afa8feec0e
kmatulud <kmatulud@student.wethinkcode.co.za>
2021-05-28 12:03:34 +0200

    fix bugs

--- greetDom.js
+++ greetDom.js
@@ -1,5 +1,4 @@
 var textfield = document.querySelector('#yourName');
-var langRadioBtn = document.querySelector('#language');
 var submitBtn = document.querySelector('.submit');
 var resetBtn = document.querySelector('#reset');
 var greetText = document.querySelector('.greeting');
@@ -26,18 +25,13 @@ function greetings(){
     greetFactory.getLanguage();
     greetFactory.setTheName(textfield.value);
     greetFactory.setGreetMessage();
-
- function incrementCounter(){
-        counter++;
-        localStorage.setItem("stores", counter);
-        counter = localStorage.getItem("stores");
-        count.innerHTML =  counter;
-    }
    
     if (greetFactory.getTheName()  !== '' && greetFactory.getTheName().match(/^[a-zA-Z]{3,15}$/gi)){
         greetText.innerHTML = greetFactory.getGreetMessage() + ' ' + greetFactory.getTheName();
         greetText.style.color = 'green';
-        incrementCounter();
+        setTimeout(function(){
+           greetText.innerHTML = '';
+        }, 8000)
     }
     else{
         greetText.innerHTML = "Please enter a valid name!"
@@ -59,15 +53,24 @@ function greetings(){
     
     greetFactory.setNamesGreeted(emptyArr);
     greetFactory.getNamesGreeted();
-
-    if(greetFactory.getTheName() != '' && !greetFactory.getNamesGreeted().includes(greetFactory.getTheName()) && textfield.value.match(/^[a-zA-Z]{3,15}$/ig) && document.querySelector("input[name='theRadio']:checked")){
-        greetFactory.checkNameExist();
-        localStorage.setItem('emptyArr', JSON.stringify(greetFactory.getNamesGreeted()));
-        incrementCounter();
+    if(greetFactory.getTheName() != '' && !greetFactory.getNamesGreeted().includes(greetFactory.getTheName()) && textfield.value.match(/^[a-zA-Z]{3,15}$/ig) && languages.checked === true){
+        localStorage.setItem('emptyArr', JSON.stringify(greetFactory.checkNameExist()));
+        counter++;
+        localStorage.setItem("stores", counter);
+        counter = localStorage.getItem("stores");
         count.innerHTML = JSON.parse(localStorage.getItem('emptyArr')).length;
+     }
+    else{
+        greetText.innerHTML = "That name has already been greeted!"
+        setTimeout(function(){
+           greetText.innerHTML = '';
+        }, 8000)
+        greetText.style.color = 'orange';
     }
 
+
     textfield.value = '';
+    languages.checked = false;
 }
 
 submitBtn.addEventListener('click', greetings);
